7191aae5c0370745dd056d640937e07f119cd49b
unknown <anthonykhodanian1@gmail.com>
2021-06-17 09:22:31 -0700

    Round 2

---
+++ Program2.py
@@ -0,0 +1,15 @@
+print("hello world again!!!")
+
+bool = False
+pool = False
+cool = False
+tool = False
+
+if(bool):
+    print("hello again")
+elif(pool):
+    print("guess not!")
+elif(cool):
+    print("cool cool!")
+else:
+    print(tool)
