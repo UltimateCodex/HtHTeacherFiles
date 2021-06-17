b85e342758334be4c44669686670d6a021ec4d78
unknown <anthonykhodanian1@gmail.com>
2021-06-17 12:33:39 -0700

    Functions and Loops wooo!

---
+++ Program4.py
@@ -0,0 +1,22 @@
+
+def fibonacciFinder (max):
+    i = 0
+    j = 1
+    k = 0
+    while i < max:
+        print(k)
+        k = i + j 
+        i = j
+        j = k
+    return("Fibonacci yay!")
+
+def primeFinder (max):
+    for i in range(2,max):
+        for j in range(2,i):
+            if i % j == 0:
+                break
+        else:
+            print(i)
+    return(max)
+
+print(fibonacciFinder(100), primeFinder(15))
